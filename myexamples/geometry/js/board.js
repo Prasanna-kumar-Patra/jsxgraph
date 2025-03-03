cedaaaa8d8381f3a41887d69bc6f03c87ac51c0a
PrasannaP <mail2lulu@gmail.com>
2025-03-03 16:56:17 +0530

    add svgs 1

--- myexamples/geometry/js/board.js
+++ myexamples/geometry/js/board.js
@@ -2,17 +2,42 @@ class GeometryBoard {
     constructor() {
         this.board = JXG.JSXGraph.initBoard('jxgbox', CONFIG.board);
         this.currentTool = null;
+        this.toolStatus = CONFIG.tools.status.PENDING;
         this.tempPoints = [];
+        this.tempObjects = [];
+        this.dragPoint = null;
+        this.touchStartTime = 0;
+        this.lastTapTime = 0;
+        
+        // History for undo/redo
+        this.history = [];
+        this.historyIndex = -1;
+        
         this.setupEventHandlers();
+        this.setupTouchHandlers();
     }
 
     setupEventHandlers() {
-        // Board click handler
+        // Mouse event handlers
         this.board.on('down', (e) => {
             if (!this.currentTool) return;
             
             const coords = this.board.getUsrCoordsOfMouse(e);
-            this.handleToolAction(coords);
+            this.startToolAction(coords);
+        });
+
+        this.board.on('move', (e) => {
+            if (!this.currentTool || this.toolStatus !== CONFIG.tools.status.ACTIVE) return;
+            
+            const coords = this.board.getUsrCoordsOfMouse(e);
+            this.updateToolAction(coords);
+        });
+
+        this.board.on('up', (e) => {
+            if (!this.currentTool || this.toolStatus !== CONFIG.tools.status.ACTIVE) return;
+            
+            const coords = this.board.getUsrCoordsOfMouse(e);
+            this.completeToolAction(coords);
         });
 
         // Setup zoom buttons
@@ -32,98 +57,423 @@ class GeometryBoard {
         this.board.update();
     }
 
-    handleToolAction(coords) {
+    setupTouchHandlers() {
+        const hammer = new Hammer(this.board.containerObj);
+        
+        hammer.get('pan').set({ direction: Hammer.DIRECTION_ALL });
+        hammer.get('pinch').set({ enable: true });
+        
+        hammer.on('tap', (e) => {
+            const now = Date.now();
+            if (now - this.lastTapTime < CONFIG.touch.doubleTapTimeout) {
+                this.handleDoubleTap(e);
+            } else {
+                this.handleTap(e);
+            }
+            this.lastTapTime = now;
+        });
+        
+        hammer.on('panstart', (e) => this.handleTouchStart(e));
+        hammer.on('panmove', (e) => this.handleTouchMove(e));
+        hammer.on('panend', (e) => this.handleTouchEnd(e));
+        
+        hammer.on('pinchstart', (e) => this.handlePinchStart(e));
+        hammer.on('pinchmove', (e) => this.handlePinchMove(e));
+        hammer.on('pinchend', (e) => this.handlePinchEnd(e));
+    }
+
+    startToolAction(coords) {
+        if (this.toolStatus !== CONFIG.tools.status.PENDING) return;
+        
+        this.toolStatus = CONFIG.tools.status.ACTIVE;
+        this.clearTempObjects();
+        
         switch (this.currentTool) {
             case 'point':
-                this.createPoint(coords[0], coords[1]);
+                this.startPointCreation(coords);
                 break;
             case 'segment':
-                this.handleSegmentTool(coords);
-                break;
             case 'line':
-                this.handleLineTool(coords);
+            case 'ray':
+                this.startLineCreation(coords);
                 break;
             case 'circle':
-                this.handleCircleTool(coords);
+                this.startCircleCreation(coords);
                 break;
             case 'polygon':
-                this.handlePolygonTool(coords);
+                this.startPolygonCreation(coords);
                 break;
+            // Add other tools here
         }
     }
 
-    createPoint(x, y) {
-        return this.board.create('point', [x, y], {
+    updateToolAction(coords) {
+        switch (this.currentTool) {
+            case 'point':
+                this.updatePointCreation(coords);
+                break;
+            case 'segment':
+            case 'line':
+            case 'ray':
+                this.updateLineCreation(coords);
+                break;
+            case 'circle':
+                this.updateCircleCreation(coords);
+                break;
+            case 'polygon':
+                this.updatePolygonCreation(coords);
+                break;
+            // Add other tools here
+        }
+    }
+
+    completeToolAction(coords) {
+        if (this.toolStatus !== CONFIG.tools.status.ACTIVE) return;
+        
+        let created = false;
+        
+        switch (this.currentTool) {
+            case 'point':
+                created = this.completePointCreation(coords);
+                break;
+            case 'segment':
+            case 'line':
+            case 'ray':
+                created = this.completeLineCreation(coords);
+                break;
+            case 'circle':
+                created = this.completeCircleCreation(coords);
+                break;
+            case 'polygon':
+                created = this.completePolygonCreation(coords);
+                break;
+            // Add other tools here
+        }
+        
+        if (created) {
+            this.addToHistory();
+        }
+        
+        this.toolStatus = CONFIG.tools.status.PENDING;
+        this.clearTempObjects();
+    }
+
+    // Point creation methods
+    startPointCreation(coords) {
+        this.dragPoint = this.board.create('point', [coords[0], coords[1]], {
             size: 4,
             name: '',
-            showInfobox: false
+            showInfobox: false,
+            fixed: false
         });
+        this.tempObjects.push(this.dragPoint);
     }
 
-    handleSegmentTool(coords) {
-        if (this.tempPoints.length === 0) {
-            this.tempPoints.push(this.createPoint(coords[0], coords[1]));
-        } else {
-            const p2 = this.createPoint(coords[0], coords[1]);
-            this.board.create('segment', [this.tempPoints[0], p2], {
-                strokeWidth: 2,
-                strokeColor: '#1a73e8'
-            });
-            this.tempPoints = [];
+    updatePointCreation(coords) {
+        if (this.dragPoint) {
+            this.dragPoint.moveTo(coords);
         }
     }
 
-    handleLineTool(coords) {
-        if (this.tempPoints.length === 0) {
-            this.tempPoints.push(this.createPoint(coords[0], coords[1]));
-        } else {
-            const p2 = this.createPoint(coords[0], coords[1]);
-            this.board.create('line', [this.tempPoints[0], p2], {
-                strokeWidth: 2,
-                strokeColor: '#1a73e8'
-            });
-            this.tempPoints = [];
-        }
+    completePointCreation(coords) {
+        if (!this.dragPoint) return false;
+        
+        const point = this.board.create('point', [coords[0], coords[1]], {
+            size: 4,
+            name: '',
+            showInfobox: false,
+            fixed: false
+        });
+        
+        return true;
     }
 
-    handleCircleTool(coords) {
-        if (this.tempPoints.length === 0) {
-            this.tempPoints.push(this.createPoint(coords[0], coords[1]));
-        } else {
-            const p2 = this.createPoint(coords[0], coords[1]);
-            this.board.create('circle', [this.tempPoints[0], p2], {
-                strokeWidth: 2,
-                strokeColor: '#1a73e8',
-                fillColor: 'none'
-            });
-            this.tempPoints = [];
-        }
-    }
-
-    handlePolygonTool(coords) {
-        const point = this.createPoint(coords[0], coords[1]);
-        this.tempPoints.push(point);
-
-        // If we have at least 3 points and clicked near the first point, complete the polygon
-        if (this.tempPoints.length >= 3) {
-            const firstPoint = this.tempPoints[0];
-            const distance = Math.sqrt(
-                Math.pow(coords[0] - firstPoint.X(), 2) + 
-                Math.pow(coords[1] - firstPoint.Y(), 2)
-            );
-
-            if (distance < 0.5) {
-                this.tempPoints.pop(); // Remove the last point
-                this.board.create('polygon', this.tempPoints, {
-                    borders: {
-                        strokeWidth: 2,
-                        strokeColor: '#1a73e8'
-                    },
-                    fillColor: '#1a73e8',
-                    fillOpacity: 0.1
+    // Line creation methods
+    startLineCreation(coords) {
+        const p1 = this.board.create('point', [coords[0], coords[1]], {
+            size: 4,
+            name: '',
+            showInfobox: false,
+            fixed: false
+        });
+        
+        const p2 = this.board.create('point', [coords[0], coords[1]], {
+            size: 4,
+            name: '',
+            showInfobox: false,
+            fixed: false
+        });
+        
+        let lineObj;
+        switch (this.currentTool) {
+            case 'segment':
+                lineObj = this.board.create('segment', [p1, p2], {
+                    strokeWidth: 2,
+                    strokeColor: '#1a73e8'
                 });
-                this.tempPoints = [];
+                break;
+            case 'line':
+                lineObj = this.board.create('line', [p1, p2], {
+                    strokeWidth: 2,
+                    strokeColor: '#1a73e8'
+                });
+                break;
+            case 'ray':
+                lineObj = this.board.create('ray', [p1, p2], {
+                    strokeWidth: 2,
+                    strokeColor: '#1a73e8'
+                });
+                break;
+        }
+        
+        this.tempPoints = [p1, p2];
+        this.tempObjects.push(p1, p2, lineObj);
+    }
+
+    updateLineCreation(coords) {
+        if (this.tempPoints.length === 2) {
+            this.tempPoints[1].moveTo(coords);
+        }
+    }
+
+    completeLineCreation(coords) {
+        if (this.tempPoints.length !== 2) return false;
+        
+        const [p1, p2] = this.tempPoints;
+        let obj;
+        
+        switch (this.currentTool) {
+            case 'segment':
+                obj = this.board.create('segment', [p1, p2], {
+                    strokeWidth: 2,
+                    strokeColor: '#1a73e8'
+                });
+                break;
+            case 'line':
+                obj = this.board.create('line', [p1, p2], {
+                    strokeWidth: 2,
+                    strokeColor: '#1a73e8'
+                });
+                break;
+            case 'ray':
+                obj = this.board.create('ray', [p1, p2], {
+                    strokeWidth: 2,
+                    strokeColor: '#1a73e8'
+                });
+                break;
+        }
+        
+        return true;
+    }
+
+    // Circle creation methods
+    startCircleCreation(coords) {
+        const center = this.board.create('point', [coords[0], coords[1]], {
+            size: 4,
+            name: '',
+            showInfobox: false,
+            fixed: false
+        });
+        
+        const radius = this.board.create('point', [coords[0], coords[1]], {
+            size: 4,
+            name: '',
+            showInfobox: false,
+            fixed: false
+        });
+        
+        const circle = this.board.create('circle', [center, radius], {
+            strokeWidth: 2,
+            strokeColor: '#1a73e8',
+            fillColor: 'none'
+        });
+        
+        this.tempPoints = [center, radius];
+        this.tempObjects.push(center, radius, circle);
+    }
+
+    updateCircleCreation(coords) {
+        if (this.tempPoints.length === 2) {
+            this.tempPoints[1].moveTo(coords);
+        }
+    }
+
+    completeCircleCreation(coords) {
+        if (this.tempPoints.length !== 2) return false;
+        
+        const [center, radius] = this.tempPoints;
+        const circle = this.board.create('circle', [center, radius], {
+            strokeWidth: 2,
+            strokeColor: '#1a73e8',
+            fillColor: 'none'
+        });
+        
+        return true;
+    }
+
+    // Polygon creation methods
+    startPolygonCreation(coords) {
+        const point = this.board.create('point', [coords[0], coords[1]], {
+            size: 4,
+            name: '',
+            showInfobox: false,
+            fixed: false
+        });
+        
+        this.tempPoints.push(point);
+        this.tempObjects.push(point);
+        
+        if (this.tempPoints.length > 1) {
+            const segment = this.board.create('segment', 
+                [this.tempPoints[this.tempPoints.length - 2], point], {
+                strokeWidth: 2,
+                strokeColor: '#1a73e8'
+            });
+            this.tempObjects.push(segment);
+        }
+    }
+
+    updatePolygonCreation(coords) {
+        if (this.tempPoints.length > 0) {
+            const lastPoint = this.tempPoints[this.tempPoints.length - 1];
+            lastPoint.moveTo(coords);
+            
+            // Show preview to first point if close enough
+            if (this.tempPoints.length > 2) {
+                const firstPoint = this.tempPoints[0];
+                const distance = Math.sqrt(
+                    Math.pow(coords[0] - firstPoint.X(), 2) + 
+                    Math.pow(coords[1] - firstPoint.Y(), 2)
+                );
+                
+                if (distance < 0.5) {
+                    if (!this.tempObjects.find(obj => obj.elType === 'segment' && 
+                        obj.point1 === lastPoint && obj.point2 === firstPoint)) {
+                        const closingSegment = this.board.create('segment', 
+                            [lastPoint, firstPoint], {
+                            strokeWidth: 2,
+                            strokeColor: '#1a73e8',
+                            dash: 2
+                        });
+                        this.tempObjects.push(closingSegment);
+                    }
+                }
             }
+        }
+    }
+
+    completePolygonCreation(coords) {
+        if (this.tempPoints.length < 3) return false;
+        
+        const firstPoint = this.tempPoints[0];
+        const lastPoint = this.tempPoints[this.tempPoints.length - 1];
+        const distance = Math.sqrt(
+            Math.pow(coords[0] - firstPoint.X(), 2) + 
+            Math.pow(coords[1] - firstPoint.Y(), 2)
+        );
+        
+        if (distance < 0.5) {
+            // Complete the polygon
+            this.board.create('polygon', this.tempPoints, {
+                borders: {
+                    strokeWidth: 2,
+                    strokeColor: '#1a73e8'
+                },
+                fillColor: '#1a73e8',
+                fillOpacity: 0.1
+            });
+            return true;
+        }
+        
+        return false;
+    }
+
+    // Touch event handlers
+    handleTap(e) {
+        const coords = this.board.getUsrCoordsOfMouse(e.center);
+        this.startToolAction(coords);
+        this.completeToolAction(coords);
+    }
+
+    handleDoubleTap(e) {
+        const coords = this.board.getUsrCoordsOfMouse(e.center);
+        if (this.currentTool === 'polygon') {
+            this.completePolygonCreation(coords);
+        }
+    }
+
+    handleTouchStart(e) {
+        const coords = this.board.getUsrCoordsOfMouse(e.center);
+        this.touchStartTime = Date.now();
+        this.startToolAction(coords);
+    }
+
+    handleTouchMove(e) {
+        const coords = this.board.getUsrCoordsOfMouse(e.center);
+        if (Date.now() - this.touchStartTime > CONFIG.touch.dragThreshold) {
+            this.updateToolAction(coords);
+        }
+    }
+
+    handleTouchEnd(e) {
+        const coords = this.board.getUsrCoordsOfMouse(e.center);
+        this.completeToolAction(coords);
+    }
+
+    handlePinchStart(e) {
+        this.lastScale = 1;
+    }
+
+    handlePinchMove(e) {
+        const factor = e.scale / this.lastScale;
+        this.zoom(factor);
+        this.lastScale = e.scale;
+    }
+
+    handlePinchEnd(e) {
+        // Nothing special to do here
+    }
+
+    // Utility methods
+    clearTempObjects() {
+        this.tempObjects.forEach(obj => {
+            this.board.removeObject(obj);
+        });
+        this.tempObjects = [];
+        this.tempPoints = [];
+        this.board.update();
+    }
+
+    addToHistory() {
+        // Remove any future states if we're in the middle of the history
+        if (this.historyIndex < this.history.length - 1) {
+            this.history = this.history.slice(0, this.historyIndex + 1);
+        }
+        
+        // Add current state to history
+        this.history.push(this.board.getBoundingBox());
+        this.historyIndex++;
+        
+        // Limit history size
+        if (this.history.length > 50) {
+            this.history.shift();
+            this.historyIndex--;
+        }
+    }
+
+    undo() {
+        if (this.historyIndex > 0) {
+            this.historyIndex--;
+            this.board.setBoundingBox(this.history[this.historyIndex]);
+            this.board.update();
+        }
+    }
+
+    redo() {
+        if (this.historyIndex < this.history.length - 1) {
+            this.historyIndex++;
+            this.board.setBoundingBox(this.history[this.historyIndex]);
+            this.board.update();
         }
     }
 
--- myexamples/geometry/js/config.js
+++ myexamples/geometry/js/config.js
@@ -1,4 +1,13 @@
 const CONFIG = {
+    // Application version
+    version: '1.0.0',
+    
+    // Touch configuration
+    touch: {
+        tapTimeout: 300,      // Maximum ms between tap start and end
+        dragThreshold: 5,     // Minimum pixels moved to start drag
+        doubleTapTimeout: 300 // Maximum ms between taps for double tap
+    },
     // Board configuration
     board: {
         boundingBox: [-20, 20, 20, -20],
@@ -14,43 +23,95 @@ const CONFIG = {
 
     // Tool categories and their tools
     tools: {
+        // Status tracking
+        status: {
+            PENDING: 'pending',   // Tool action not started
+            ACTIVE: 'active',     // Tool action in progress
+            COMPLETE: 'complete'  // Tool action completed
+        },
         basic: {
             name: "Basic Tools",
             tools: [
-                { id: "move", name: "Move", icon: "move.svg" },
-                { id: "point", name: "Point", icon: "point.svg" },
-                { id: "segment", name: "Segment", icon: "segment.svg" },
-                { id: "line", name: "Line", icon: "line.svg" },
-                { id: "polygon", name: "Polygon", icon: "polygon.svg" },
-                { id: "circle", name: "Circle", icon: "circle.svg" }
+                { id: "move", name: "Move", icon: "move.svg", cursor: "move" },
+                { id: "point", name: "Point", icon: "point.svg", cursor: "crosshair" },
+                { id: "delete", name: "Delete", icon: "delete.svg", cursor: "not-allowed" },
+                { id: "undo", name: "Undo", icon: "undo.svg" },
+                { id: "redo", name: "Redo", icon: "redo.svg" }
             ]
         },
         construct: {
             name: "Construct",
             tools: [
-                { id: "midpoint", name: "Midpoint", icon: "midpoint.svg" },
-                { id: "perpendicular", name: "Perpendicular", icon: "perpendicular.svg" },
-                { id: "parallel", name: "Parallel", icon: "parallel.svg" },
-                { id: "angle_bisector", name: "Angle Bisector", icon: "angle_bisector.svg" },
-                { id: "tangent", name: "Tangent", icon: "tangent.svg" }
+                { id: "midpoint", name: "Midpoint", icon: "midpoint.svg", cursor: "crosshair" },
+                { id: "perpendicular", name: "Perpendicular", icon: "perpendicular.svg", cursor: "crosshair" },
+                { id: "parallel", name: "Parallel", icon: "parallel.svg", cursor: "crosshair" },
+                { id: "angle_bisector", name: "Angle Bisector", icon: "angle_bisector.svg", cursor: "crosshair" },
+                { id: "tangent", name: "Tangent", icon: "tangent.svg", cursor: "crosshair" },
+                { id: "perpendicular_bisector", name: "Perpendicular Bisector", icon: "perpendicular_bisector.svg", cursor: "crosshair" },
+                { id: "locus", name: "Locus", icon: "locus.svg", cursor: "crosshair" }
             ]
         },
         measure: {
             name: "Measure",
             tools: [
-                { id: "angle", name: "Angle", icon: "angle.svg" },
-                { id: "distance", name: "Distance", icon: "distance.svg" },
-                { id: "area", name: "Area", icon: "area.svg" }
+                { id: "angle", name: "Angle", icon: "angle.svg", cursor: "crosshair" },
+                { id: "angle_fixed", name: "Angle with Given Size", icon: "angle_fixed.svg", cursor: "crosshair" },
+                { id: "distance", name: "Distance", icon: "distance.svg", cursor: "crosshair" },
+                { id: "area", name: "Area", icon: "area.svg", cursor: "crosshair" },
+                { id: "slope", name: "Slope", icon: "slope.svg", cursor: "crosshair" }
             ]
         },
+        lines: {
+            name: "Lines",
+            tools: [
+                { id: "segment", name: "Segment", icon: "segment.svg", cursor: "crosshair" },
+                { id: "segment_fixed", name: "Segment with Given Length", icon: "segment_fixed.svg", cursor: "crosshair" },
+                { id: "ray", name: "Ray", icon: "ray.svg", cursor: "crosshair" },
+                { id: "line", name: "Line", icon: "line.svg", cursor: "crosshair" },
+                { id: "vector", name: "Vector", icon: "vector.svg", cursor: "crosshair" },
+                { id: "polyline", name: "Polyline", icon: "polyline.svg", cursor: "crosshair" }
+            ]
+        },
+
+        circles: {
+            name: "Circles",
+            tools: [
+                { id: "circle", name: "Circle with Center", icon: "circle.svg", cursor: "crosshair" },
+                { id: "circle_radius", name: "Circle with Radius", icon: "circle_radius.svg", cursor: "crosshair" },
+                { id: "circle_three_points", name: "Circle through 3 Points", icon: "circle_three_points.svg", cursor: "crosshair" },
+                { id: "semicircle", name: "Semicircle", icon: "semicircle.svg", cursor: "crosshair" },
+                { id: "arc_three_points", name: "Circular Arc", icon: "arc.svg", cursor: "crosshair" },
+                { id: "sector_three_points", name: "Circular Sector", icon: "sector.svg", cursor: "crosshair" }
+            ]
+        },
+
+        polygons: {
+            name: "Polygons",
+            tools: [
+                { id: "polygon", name: "Polygon", icon: "polygon.svg", cursor: "crosshair" },
+                { id: "regular_polygon", name: "Regular Polygon", icon: "regular_polygon.svg", cursor: "crosshair" },
+                { id: "triangle", name: "Triangle", icon: "triangle.svg", cursor: "crosshair" },
+                { id: "rectangle", name: "Rectangle", icon: "rectangle.svg", cursor: "crosshair" },
+                { id: "quadrilateral", name: "Quadrilateral", icon: "quadrilateral.svg", cursor: "crosshair" }
+            ]
+        },
+
         transform: {
             name: "Transform",
             tools: [
-                { id: "translate", name: "Translate", icon: "translate.svg" },
-                { id: "rotate", name: "Rotate", icon: "rotate.svg" },
-                { id: "reflect_line", name: "Reflect Line", icon: "reflect_line.svg" },
-                { id: "reflect_point", name: "Reflect Point", icon: "reflect_point.svg" },
-                { id: "dilate", name: "Dilate", icon: "dilate.svg" }
+                { id: "translate", name: "Translate by Vector", icon: "translate.svg", cursor: "move" },
+                { id: "rotate", name: "Rotate around Point", icon: "rotate.svg", cursor: "move" },
+                { id: "reflect_line", name: "Reflect about Line", icon: "reflect_line.svg", cursor: "move" },
+                { id: "reflect_point", name: "Reflect about Point", icon: "reflect_point.svg", cursor: "move" },
+                { id: "dilate", name: "Dilate from Point", icon: "dilate.svg", cursor: "move" }
+            ]
+        },
+
+        media: {
+            name: "Media",
+            tools: [
+                { id: "text", name: "Text", icon: "text.svg", cursor: "text" },
+                { id: "image", name: "Image", icon: "image.svg", cursor: "copy" }
             ]
         }
     }
